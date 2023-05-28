8fd8abcb212ae3c04d33e5225defc780e5747bd0
Adrian Osorio <osorioadriancamilo@gmail.com>
2023-05-27 20:43:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { dbConnection } = require('./database/config');
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express();
+
+dbConnection();
+
+app.use(cors())
+
+app.use( express.static('public') )
+
+app.use( express.json());
+
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
+})
